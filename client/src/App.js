352ef8db2805142d8a4976d80bb058440e365b70
islanur <islanur@gmail.com>
2021-04-13 11:23:05 +0800

    Membuat komponen Buku

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+import Book from "./Book";
+import "./App.css";
+
 function App() {
-  const greeting = <div>Hello World!!!</div>;
-  return <div className="App">{greeting}</div>;
+  return (
+    <div className="App">
+      <Book />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/Book.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Book.css";
+
+function Book() {
+  return <div id="book-message">Hello I'm a book!</div>;
+}
+
+export default Book;
